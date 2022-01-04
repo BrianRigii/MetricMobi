7c466cc9fc8a0d151c332a892fb3f48e4d8a5180
John Brian <johnbryanjb011@gmail.com>
2022-01-04 15:29:07 +0300

    chat sreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:metric/services/auth_service.dart';
+import 'package:metric/services/error_service.dart';
+import 'package:metric/services/messaging_service.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key key}) : super(key: key);
@@ -9,9 +12,44 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+  }
+
+  void _loadMessages() {
+    messagingService.loadMessages(authService.authUser.id).catchError((error) {
+      errorService.errorHandler(error: error, context: context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Chats Come Here')),
-    );
+        appBar: AppBar(
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(top: 25, left: 8, right: 8, bottom: 8),
+              color: Theme.of(context).primaryColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Messages',
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/services/messaging_service.dart
+++ lib/services/messaging_service.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:metric/api/api.dart';
+import 'package:metric/data/models/message_model.dart';
 
 class MessagingService extends ChangeNotifier {
   bool _isGettingMessages = false;
   bool get isGettingMessages => _isGettingMessages;
+
+  List<MessageModel> messages = [];
 
   set isGettingMessages(bool val) {
     _isGettingMessages = val;
@@ -11,8 +14,21 @@ class MessagingService extends ChangeNotifier {
   }
 
   Future loadMessages(userId) {
+    messages.clear();
     _isGettingMessages = true;
-    return api.getMessages(userId).then((response) {});
+    return api.getMessages(userId).then((response) {
+      var payload = response.data;
+      _saveMessages(payload);
+    }).catchError((error) {
+      return throw error;
+    });
+  }
+
+  void _saveMessages(payload) {
+    payload.forEach((msg) {
+      messages.add(MessageModel.fromMap(msg));
+    });
+    isGettingMessages = false;
   }
 }
 
